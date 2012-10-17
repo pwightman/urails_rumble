d634e56a996e809c7e69e489514dc48296593c7e
Parker Wightman <parkerwightman@gmail.com>
2012-10-17 12:25:57 -0600

    Removing length requirement as regex makes it unnecessary.

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -2,7 +2,6 @@ class Pledge < ActiveRecord::Base
   attr_accessible :email
 
   validates :email, :presence => true, 
-                    :length => {:minimum => 3, :maximum => 254},
                     :uniqueness => true,
                     :format => {:with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i}
 
