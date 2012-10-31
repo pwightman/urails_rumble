8b00eda17b704e3c0608f1d0cb7ac39a28a6bf06
Parker Wightman <parkerwightman@gmail.com>
2012-10-31 13:45:20 -0600

    Fixing github login to use proper client id and secret.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,12 @@
+if Rails.env.production?
+  client_id     = ENV['OAUTH_GITHUB_CLIENT_ID']
+  client_secret = ENV['OAUTH_GITHUB_CLIENT_SECRET']
+else
+  client_id     = '576f701805248d850c50'
+  client_secret = '57d95cc84b3a9512b511d9fd1d782398d9bc845a'
+end
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :github, '576f701805248d850c50', '57d95cc84b3a9512b511d9fd1d782398d9bc845a'
+  provider :github, client_id, client_secret
 end
