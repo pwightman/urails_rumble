d4ee7d6a17579ad4c8b4622c5adab85c6b1cf1e6
Parker Wightman <parkerwightman@gmail.com>
2012-11-12 16:00:30 -0700

    Adding routes for members to do things.

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,4 +2,27 @@ class TeamsController < ApplicationController
   def index
     @teams = Team.all
   end
+
+  def create
+    # Hoa
+  end
+
+  def new
+    redirect_to root_path if current_user.nil?
+    # Hoa
+  end
+
+  def join
+    @team = Team.find(params[:id])
+    # Kingsley adds logic for adding member to team
+    # Use 'current_user' as the logged-in user to assign
+    # to this team.
+    # Be sure to to error check things like:
+    #  * There isn't more than 4 people on the team
+  end
+
+  def show
+    @team = Team.find(params[:id])
+    # Landon
+  end
 end
--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -1,13 +1,11 @@
 class Authentication < ActiveRecord::Base
    belongs_to :user
    
-   attr_accessible :uid, :provider
-   
-def provider_name
-  if provider == 'open_id'
-    "OpenID"
-  else
-    provider.titleize
+  def provider_name
+    if provider == 'open_id'
+      "OpenID"
+    else
+      provider.titleize
+    end
   end
-end
 end
