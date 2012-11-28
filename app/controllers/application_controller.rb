94f3c6422db9a46d0cfd5882b93c13692217154e
Parker Wightman <parkerwightman@gmail.com>
2012-11-28 15:31:48 -0700

    Using find_by_id so nil is returned instead of throwing an exception.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,6 @@ helper_method :current_user
   private
 
  def current_user
-   @current_user ||= User.find(session[:user_id]) if session[:user_id]
+   @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
  end
 end
