441ab4f88c1314795ca60f29952b96903fddfcd7
Parker Wightman <parkerwightman@gmail.com>
2012-11-30 20:01:42 -0700

    Adding login_with_oauth logic for testing mailers.

--- spec/requests/teams_mailer_request_spec.rb
+++ spec/requests/teams_mailer_request_spec.rb
@@ -3,9 +3,14 @@ require 'spec_helper'
 describe "TeamMailers" do
   let(:team) { Factory(:team) }
 
+  before :each do
+    team.users << omniauth_user
+    login_with_oauth
+  end
+
   it "does not send an email when email field is blank" do
     visit team_path(team)
-    click_button "Invite"
+    click_button "Send Invite"
     sent_email.should be_empty
     page.should have_content("The email address you entered is not valid")
   end
@@ -14,7 +19,7 @@ describe "TeamMailers" do
     email = "test@example.com"
     visit team_path(team)
     fill_in "email", with: email
-    click_button "Invite"
+    click_button "Send Invite"
     sent_email.should_not be_empty
     page.should have_content("Invite has been sent")
   end
@@ -23,7 +28,7 @@ describe "TeamMailers" do
     email = "test@example"
     visit team_path(team)
     fill_in "email", with: email
-    click_button "Invite"
+    click_button "Send Invite"
     sent_email.should_not include(email)
     page.should have_content("The email address you entered is not valid")
   end
